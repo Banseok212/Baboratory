cb7f1c168802b70717784b04765382a548ec8689
Banseok212 <87890283+Banseok212@users.noreply.github.com>
2024-06-21 14:07:06 +0900

    Update vcfSort.sh

--- vcfSort.sh
+++ vcfSort.sh
@@ -1,13 +1,19 @@
 #!/bin/sh
 
-#vcf 파일에서 CHROM과 POS정보를 가지고 해당 행만 추출, vcf 저장
+#final_output.vcf 파일에서 내가 원하는 CHROM, POS 행만 추출, vcf 저장
+#chrom_post.csv: 1열에 CHROM, 2열에 POS 정보가 존재하는 csv 파일
+#ex) less chrom_post.csv
+#    1    chr1    11854476
+#    2    chr1    11188756
+#    3    chr2    15847393
+#    ...
 
 # 입력 VCF 파일 경로
 input_vcf_file="final_output.vcf"
 # 출력 VCF 파일 경로
 output_vcf_file="filtered_output.vcf"
 # 입력 CSV 파일 경로
-input_csv_file="chrom_post.csv"
+input_csv_file="chrom_post.csv" 
 
 # awk 조건 생성
 awk_conditions=""
